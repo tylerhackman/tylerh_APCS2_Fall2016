d06a811a88171338fc9f309ae9e658ad13da3620
tylerhackman <tyhackman@ymail.com>
2016-08-31 11:04:51 -0700

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	.metadata/.mylyn/.tasks.xml.zip
    #	.metadata/.mylyn/tasks.xml.zip
    
    #	.metadata/.plugins/org.eclipse.core.resources/.projects/HelloWorld/.markers
    
    #	.metadata/.plugins/org.eclipse.core.resources/.projects/HelloWorld/.markers.snap
    
    #	.metadata/.plugins/org.eclipse.core.resources/.projects/HelloWorld/.syncinfo.snap
    
    #	.metadata/.plugins/org.eclipse.core.resources/.projects/Pikachu/.indexes/e4/history.index
    
    #	.metadata/.plugins/org.eclipse.core.resources/.projects/Pikachu/.markers
    
    #	.metadata/.plugins/org.eclipse.core.resources/.projects/Pikachu/.markers.snap
    
    #	.metadata/.plugins/org.eclipse.core.resources/.projects/Pikachu/.syncinfo.snap
    
    #	.metadata/.plugins/org.eclipse.core.resources/.root/.indexes/properties.index
    #	.metadata/.plugins/org.eclipse.core.resources/.root/.markers.snap
    
    #	.metadata/.plugins/org.eclipse.core.resources/.safetable/org.eclipse.core.resources
    
    #	.metadata/.plugins/org.eclipse.core.runtime/.settings/org.eclipse.jdt.launching.prefs
    #	.metadata/.plugins/org.eclipse.e4.workbench/workbench.xmi
    #	.metadata/.plugins/org.eclipse.jdt.core/3240577434.index
    #	.metadata/.plugins/org.eclipse.jdt.core/externalFilesCache
    #	.metadata/.plugins/org.eclipse.jdt.core/nonChainingJarsCache
    #	.metadata/.plugins/org.eclipse.jdt.core/savedIndexNames.txt
    #	.metadata/.plugins/org.eclipse.jdt.ui/dialog_settings.xml
    #	.metadata/.plugins/org.eclipse.jdt.ui/jdt-images/0.png

---
+++ Calculate Library/Calculate.java
@@ -0,0 +1,10 @@
+
+public class Calculate {
+	
+	public static int square(int number) {
+		return(number * number);
+	}	
+	public static int cube(int number) {
+		return(number * number * number);
+	}
+}
