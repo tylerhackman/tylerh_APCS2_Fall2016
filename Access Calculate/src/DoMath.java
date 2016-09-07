9e256ac9eea508a934cfb4c78872e08f2e18ed0d
tylerhackman <tyhackman@ymail.com>
2016-09-06 20:06:11 -0700

    Math Library Part 2

--- Access Calculate/src/DoMath.java
+++ Access Calculate/src/DoMath.java
@@ -2,10 +2,11 @@
 public class DoMath {
 
 	public static void main(String[] args) {
-		Calculate.square(5);
-		Calculate.cube(5);
-		Calculate.average(5);
+	
+		System.out.println(Calculate.round2(4.62375));
+
 	}
 	
 
 }
+
