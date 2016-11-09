8972e45c0b67764fc0892e3f6990abb4c4f01010
tylerhackman <tyhackman@ymail.com>
2016-11-08 18:37:22 -0800

    Frac Calc Part 1

--- FracCalc/src/fracCalc/FracCalc.java
+++ FracCalc/src/fracCalc/FracCalc.java
@@ -1,11 +1,14 @@
-package fracCalc;
-
+ package fracCalc;
+import java.util.*;
 public class FracCalc {
 
     public static void main(String[] args) 
     {
         // TODO: Read the input from the user and call produceAnswer with an equation
-
+    	Scanner userInput = new Scanner(System.in);
+    	System.out.println("Enter fraction string.");
+    	String input = userInput.nextLine();
+    	System.out.println(produceAnswer(input));
     }
     
     // ** IMPORTANT ** DO NOT DELETE THIS FUNCTION.  This function will be used to test your code
@@ -19,8 +22,10 @@ public class FracCalc {
     public static String produceAnswer(String input)
     { 
         // TODO: Implement this function to produce the solution to the input
-        
-        return "";
+        String firstOperand = input.substring(0, input.indexOf(" "));
+        String operator = input.substring((input.indexOf(" ")+ 1)); 
+        String secondOperand = input.substring(input.lastIndexOf(" ") + 1);
+        return secondOperand;
     }
 
     // TODO: Fill in the space below with any helper methods that you think you will need
